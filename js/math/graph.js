ab43efc1c73142a6736c76f7076aa3fc607a4536
Jung-GunSong <jsong781@gmail.com>
2023-12-26 19:12:47 -0500

    Added methods to allow adding of random points
    Added method to attempt to add a point to the graph if it is
    not already a point that exists. Added methods to support
    this method as well.

--- js/math/graph.js
+++ js/math/graph.js
@@ -15,7 +15,48 @@ class Graph {
   }
 
   /**
-   * method that will draw the points and segements
+   * Adds a new point to the graph
+   *
+   * args: point as new point to be added
+   *
+   */
+  addPoint(point){
+    this.points.push(point);
+  }
+
+  /**
+   * method to that will add a point to
+   * this graph if it doesn't already exist and return true;
+   * else will return false
+   *
+   * args: point as Point obj
+   *
+   * return: true or false
+   */
+  tryAddPoint(point){
+    if (!this.containsPoint(point)){
+      this.addPoint(point);
+      return true;
+    }else {
+      return false
+    }
+
+  }
+
+  /**
+   * method to determine if a point already exists
+   * as a point in the graph
+   *
+   * args: point as Point obj
+   *
+   * return: true or false
+   */
+  containsPoint(point){
+    return this.points.find((p) => p.equals(point));
+  }
+
+  /**
+   * method that will draw the points and segments
    * that are defined as args for the class
    *
    * args: context as the canvas where the graph is
